b2b7416c8861730a759eebd84891ad32d77e69a7
julianluisasoto <julianlasoto@hotmail.com>
2023-04-02 22:55:03 -0300

    add logo to home

--- src/client/src/Components/Home.jsx
+++ src/client/src/Components/Home.jsx
@@ -6,9 +6,13 @@ export const Home = () => {
   const name = user.name;
   return (
     <div>
-      <h1 className="text-center text-sky-950">
-        BIENVENIDO {name.toUpperCase()} A
-      </h1>
+      <div className="flex justify-between">
+        <h1 className="text-center text-sky-950">
+          BIENVENIDO {name.toUpperCase()} A
+        </h1>
+        <img src="https://imageup.me/u8f" alt="logo-gif" />
+      </div>
+
       <h1 className="flex direction justify-center text-2xl">I-CLEAN</h1>
       <h2 className="flex direction justify-center text-xl text-orange-600 font-bold">
         El mejor sitio para contratar Empleadas Domesticas
