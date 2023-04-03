fa79099a4a2e5ff3681458af8a223f63d2f13b44
julianluisasoto <julianlasoto@hotmail.com>
2023-04-02 21:31:21 -0300

    deploying backend and DB

--- src/api/database.js
+++ src/api/database.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 //const URI = "mongodb://0.0.0.0:27017";
 mongoose
-  .connect("mongodb://0.0.0.0:27017")
+  .connect(
+    "mongodb://mongo:7xq1s5gnipQgB4bQeNMv@containers-us-west-125.railway.app:6174" ||
+      "mongodb://0.0.0.0:27017"
+  )
   .then((db) => console.log("im conected to DB"))
   .catch((error) => console.log(error));
 
