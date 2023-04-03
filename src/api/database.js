c79e7042fdc8c21cef54cffd49658cf0f9b06282
julianluisasoto <julianlasoto@hotmail.com>
2023-04-02 22:00:47 -0300

    FIX NODE START

--- src/api/database.js
+++ src/api/database.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
 //const URI = "mongodb://0.0.0.0:27017";
+const URI =
+  "mongodb://mongo:7xq1s5gnipQgB4bQeNMv@containers-us-west-125.railway.app:6174";
 mongoose
-  .connect(
-    "mongodb://mongo:7xq1s5gnipQgB4bQeNMv@containers-us-west-125.railway.app:6174" ||
-      "mongodb://0.0.0.0:27017"
-  )
+  .connect(process.env.URI || "mongodb://0.0.0.0:27017")
   .then((db) => console.log("im conected to DB"))
   .catch((error) => console.log(error));
 
